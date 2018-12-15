8d37311072fa1a0fe7a4f89933b3ab787fd3751d
MATAN\matan <matany90@gmail.com>
2018-12-15 15:14:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => { // הסלאש מייצג את היו-אר-רל שבו יבוצע הפאט ארו פאנקשיין
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000; // אם אנחנו בסביבת פיתוח תן לי את הפורט שחוזר מהירוקו אחרת שים אותי על פורט 5000
+app.listen(PORT);
