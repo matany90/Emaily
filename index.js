db04f0909823b34541c914c42ac0c8d10e98cb0c
MATAN\matan <matany90@gmail.com>
2018-12-15 15:34:02 +0200

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => { // הסלאש מייצג את היו-אר-רל שבו יבוצע הפאט ארו פאנקשיין
-    res.send({hi: 'there'});
+    res.send({hi: 'there1234'});
 });
 
 const PORT = process.env.PORT || 5000; // אם אנחנו בסביבת פיתוח תן לי את הפורט שחוזר מהירוקו אחרת שים אותי על פורט 5000
