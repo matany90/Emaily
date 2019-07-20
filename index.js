00d368b4de7e5201696a0007b6596a7331fef4e9
MATAN\matan <matany90@gmail.com>
2019-07-20 11:54:21 +0300

    test1234

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req,res) => { // הסלאש מייצג את היו-אר-רל שבו יבוצע הפאט ארו פאנקשיין
-    res.send({hi: 'there1234'});
-});
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000; // אם אנחנו בסביבת פיתוח תן לי את הפורט שחוזר מהירוקו אחרת שים אותי על פורט 5000
 app.listen(PORT);
+
+//auth flow:
+//1 - app.get 'auth/google'
+//2 - app.get 'auth/google/callback
+//3 - הarrow function  נקראת מהgoogle strategy
